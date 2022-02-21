17773f204fec97d0064819c98adbc7bf0c9fc991
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-22 12:13:47 +1300

    create, access item, print out with for loop

---
+++ list-of-lists1.py
@@ -0,0 +1,19 @@
+# create list
+client_list = [ ['John Smith', 16], ['Bob Roberts', 18], ['Tui Ramere', 25] ]
+
+print(client_list)
+
+# get 1st client's name
+name= client_list[0][0]
+
+print(name)
+
+# get 2nd client's age
+age = client_list[1][1]
+
+print(age)
+
+
+# for with temporary variable
+for client in client_list:
+    print(f"Name: {client[0]} Age: {client[1]}")
