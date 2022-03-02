b0b97afd235a563837be4042762996e12e781567
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-03-02 14:33:46 +1300

    simple function with return value

--- functions-demo2.py
+++ functions-demo2.py
@@ -3,12 +3,15 @@
 
 def to_the_power(a, b):
     answer = a**b
+    return answer
+    
+
 
 print("Enter a number:")
 num1 = int(input())
 print("Enter a second number:")
 num2 = int(input())
 
-to_the_power(num1, num2)
+result = to_the_power(num1, num2)
 
-print(f"{num1} to the power of {num2} is {answer}")
+print(f"{num1} to the power of {num2} is {result}")
