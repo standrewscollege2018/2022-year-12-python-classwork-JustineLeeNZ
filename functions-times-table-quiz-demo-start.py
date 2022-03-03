9b2e93805d8efc9778fafdc2a4d0300e1fdaf25f
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-03-03 14:19:49 +1300

    added function

--- functions-times-table-quiz-demo-start.py
+++ functions-times-table-quiz-demo-start.py
@@ -1,32 +1,35 @@
 ''' learning about functions - times tables quiz demo - Raspberry Pi Foundation - adapted by Ms Lee 28/2/2022'''
 
-''' this code runs a basic times tables quiz, 
-asks user which times table, then which value to go up to.
-Then asks user to answer questions for chosen times table.
-Includes some error handling '''
+''' this code runs a basic times tables quiz, asks user which times table, then which value to go up to.
+Then asks user to answer questions for chosen times table. '''
 
+def check_integer():
+  not_validated = True
+  
+  while not_validated:
+    try:
+      num1 = int(input())
+      not_validated = False
+    except ValueError:
+      print("You must enter a whole number: ")
+      
+    return num1
+
+## - main program
 print("Welcome to the times table quiz")
 
 # get choice of times table
-not_validated = True
-while not_validated:
-  try:
-    print("Enter a times table that you would like to be tested on:")
-    times_table = int(input())
-    not_validated = False
-  except ValueError:
-    print("You must enter a whole number: ")
+print("Enter a times table that you would like to be tested on:")
+times_table = check_integer()
+
 
 # get maximum value for chosen times table    
-not_validated = True
-while not_validated:
-  print("Enter the maximum value for your times table: ")
-  try:
-    max_value = int(input())
-    not_validated = False
-  except ValueError:
-    print("You must enter a whole number: ")
-    
+
+print("Enter the maximum value for your times table: ")
+
+max_value = check_integer()
+ 
+# add 1 so that max value is included in quiz    
 max_value = max_value + 1
 answer = 0
 
